6990382e128b84cd69b525b58680990654dfc972
ErinaYip <erinayip@outlook.com>
2025-11-10 16:40:17 +0800

    fixed light/dark toggler for mermaid theme

--- src/utils/setting-utils.ts
+++ src/utils/setting-utils.ts
@@ -45,10 +45,7 @@ export function applyThemeToDocument(theme: LIGHT_DARK_MODE) {
 	}
 
 	// Set the theme for Expressive Code
-	document.documentElement.setAttribute(
-		"data-theme",
-		expressiveCodeConfig.theme,
-	);
+	document.documentElement.setAttribute("data-theme", theme);
 }
 
 export function setTheme(theme: LIGHT_DARK_MODE): void {
